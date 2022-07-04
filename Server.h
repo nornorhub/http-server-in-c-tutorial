7a6a779d9a5ecd352b269516d806c1b664b4aec8
Noureldin Elhelw <noureldinelhelw14@gmail.com>
2022-07-04 03:19:20 -0400

    Initial Commit

---
+++ Server.h
@@ -0,0 +1,27 @@
+#ifndef Server_h
+#define Server_h
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+struct Server
+{
+    int domain;
+    int protocol;
+    int service;
+    unsigned long interface;
+    int port;
+    int backlog;
+
+    struct sockaddr_in address;
+
+    int socket;
+
+    void (*launch)(struct Server *server);
+};
+
+struct Server server_constructor(int domain, int service,
+                                 int protocol, unsigned long interface_, int port,
+                                 int backlog, void (*launch)(struct Server *server));
+
+#endif
